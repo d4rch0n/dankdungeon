5857d8c64c3e644c23ef93a4f4012df4068c539f
Johan Nestaas <johannestaas@gmail.com>
2017-09-20 01:32:00 -0700

    interference patterns! not what i wanted but cool

--- dankdungeon/worldmap.py
+++ dankdungeon/worldmap.py
@@ -1,4 +1,29 @@
+import math
+from math import floor
+import random
 from PIL import Image
+
+
+class Sin3D:
+
+    def __init__(self, coefx, coefy, subx, suby):
+        self.coefx = coefx
+        self.coefy = coefy
+        self.subx = subx
+        self.suby = suby
+
+    def __call__(self, x, y):
+        return (
+            math.sin(self.coefx * (x - self.subx)) +
+            math.sin(self.coefy * (y - self.suby))
+        )
+
+    @classmethod
+    def random(cls, octave):
+        p = 1 / (2**octave)
+        subx = random.uniform(0, math.pi * 2)
+        suby = random.uniform(0, math.pi * 2)
+        return cls(p, p, subx, suby)
 
 
 class WorldMap:
@@ -9,7 +34,22 @@ class WorldMap:
         self.px = self.im.load()
 
     def generate(self):
-        pass
+        self.funcs = []
+        for o in range(1, 7):
+            f = Sin3D.random(o)
+            self.funcs.append(f)
+        w, h = self.size
+        for x in range(w):
+            for y in range(h):
+                self.px[x, y] = self.calc_pt_color(x, y)
+
+    def calc_pt(self, x, y):
+        vals = [f(x, y) for f in self.funcs]
+        return sum(vals)
+
+    def calc_pt_color(self, x, y):
+        ht = self.calc_pt(x, y)
+        return (floor(256 * ht), floor(256 * ht), floor(256 * ht))
 
     def debug(self):
         pass
