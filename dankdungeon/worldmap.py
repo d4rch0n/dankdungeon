ffa1f7294d2069c74770e542f3ae50210cf83e4e
Johan Nestaas <johannestaas@gmail.com>
2017-09-20 02:28:38 -0700

    generate perlin noise

--- dankdungeon/worldmap.py
+++ dankdungeon/worldmap.py
@@ -1,42 +1,47 @@
-import math
-from math import floor
 import random
+from math import floor
 from PIL import Image
 
+random.seed()
 
-class Sin3D:
 
-    def __init__(self, coefx, coefy, subx, suby):
-        self.coefx = coefx
-        self.coefy = coefy
-        self.subx = subx
-        self.suby = suby
+class Noise:
+
+    def __init__(self, octave, size):
+        self.w = size[0]
+        self.h = size[1]
+        self.s = 2**octave
+        self.m = []
+        p = 1 / self.s
+        for x in range(self.s):
+            n = []
+            for y in range(self.s):
+                n.append(random.randint(0, 1) * p)
+            self.m.append(n)
 
     def __call__(self, x, y):
-        return (
-            math.sin(self.coefx * (x - self.subx)) +
-            math.sin(self.coefy * (y - self.suby))
-        )
+        xx = x / self.w
+        yy = y / self.h
+        px = floor(self.s * xx)
+        py = floor(self.s * yy)
+        return self.m[px][py]
 
     @classmethod
-    def random(cls, octave):
-        p = 1 / (2**octave)
-        subx = random.uniform(0, math.pi * 2)
-        suby = random.uniform(0, math.pi * 2)
-        return cls(p, p, subx, suby)
+    def random(cls, octave, size):
+        return cls(octave, size)
 
 
 class WorldMap:
 
-    def __init__(self, width=1024, height=1024):
+    def __init__(self, width=256, height=256):
         self.size = (width, height)
         self.im = Image.new('RGB', self.size)
         self.px = self.im.load()
 
     def generate(self):
         self.funcs = []
-        for o in range(1, 7):
-            f = Sin3D.random(o)
+        for o in range(1, 10):
+            f = Noise.random(o, self.size)
             self.funcs.append(f)
         w, h = self.size
         for x in range(w):
@@ -49,7 +54,15 @@ class WorldMap:
 
     def calc_pt_color(self, x, y):
         ht = self.calc_pt(x, y)
-        return (floor(256 * ht), floor(256 * ht), floor(256 * ht))
+        # if ht > 0.7:
+        #     ht = 1.0
+        # else:
+        #     ht = 0
+        return (
+            256 - floor(256 * ht),
+            256 - floor(256 * ht),
+            256 - floor(256 * ht),
+        )
 
     def debug(self):
         pass
@@ -57,17 +70,22 @@ class WorldMap:
     def show(self):
         self.im.show()
 
+    def save(self, path):
+        self.im.save(path)
+
 
 def main_worldmap():
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--debug', '-d', action='store_true')
+    parser.add_argument('--out', '-o', default='worldmap.png')
     args = parser.parse_args()
     wm = WorldMap()
     wm.generate()
     if args.debug:
         wm.debug()
     wm.show()
+    wm.save(args.out)
 
 
 if __name__ == '__main__':
